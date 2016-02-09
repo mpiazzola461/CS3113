fa5157810d66c41359bc0999ded23d7dd020a2a7
mpiazzola461 <mmp461@nyu.edu>
2016-02-09 12:46:59 -0500

    Homework 1

---
+++ Homework 1/NYUCodebase/main.cpp
@@ -0,0 +1,130 @@
+/*
+Marcus Piazzola
+CS 3113 Ivan Safrin Intro to Game Programming
+Assignment 1
+*/
+
+#ifdef _WINDOWS
+	#include <GL/glew.h>
+#endif
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+#include "Matrix.h"
+#include "ShaderProgram.h"
+#ifdef _WINDOWS
+	#define RESOURCE_FOLDER ""
+#else
+	#define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
+#endif
+
+SDL_Window* displayWindow;
+
+
+GLuint LoadTexture(const char *image_path) {
+	SDL_Surface *surface = IMG_Load(image_path);
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_RGBA,
+		GL_UNSIGNED_BYTE, surface->pixels);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	SDL_FreeSurface(surface);
+	return textureID;
+}
+
+
+int main(int argc, char *argv[])
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	displayWindow = SDL_CreateWindow("Marcus Piazzola Scene", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
+	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+	SDL_GL_MakeCurrent(displayWindow, context);
+	#ifdef _WINDOWS
+		glewInit();
+	#endif
+
+	SDL_Event event;
+	bool done = false;
+
+	glViewport(0, 0, 640, 360);
+	ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
+	GLuint text1 = LoadTexture("cardSpadesA.png");
+	GLuint text2 = LoadTexture("cardHeartsA.png");
+	GLuint text3 = LoadTexture("cardDiamondsA.png");
+
+	Matrix projectionMatrix;
+	Matrix modelMatrix;
+	Matrix viewMatrix;
+	projectionMatrix.setOrthoProjection(-3.55, 3.55, -2.0f, 2.0f, -1.0f, 1.0f);
+	float lastFrameTicks = 0.0f;
+	glUseProgram(program.programID);
+
+	while (!done) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+				done = true;
+			}
+		}
+
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+
+		float ticks = (float)SDL_GetTicks() / 1000.0f;
+		float elapsed = ticks - lastFrameTicks;
+		lastFrameTicks = ticks;
+
+		program.setProjectionMatrix(projectionMatrix);
+		program.setViewMatrix(viewMatrix);
+		program.setModelMatrix(modelMatrix);
+
+		//Card 1 (Ace of Hearts)
+		float vertices1[] = { -2.0, -0.5, -1.0, -0.5, -1.0, 0.5, -2.0, -0.5, -1.0, 0.5, -2.0, 0.5 };
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices1);
+		glEnableVertexAttribArray(program.positionAttribute);
+		float texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+		glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+		glEnableVertexAttribArray(program.texCoordAttribute);
+		glBindTexture(GL_TEXTURE_2D, text2);
+		glDrawArrays(GL_TRIANGLES, 0, 6);	
+		glDisableVertexAttribArray(program.positionAttribute);
+		glDisableVertexAttribArray(program.texCoordAttribute);
+
+
+		//Card 2 (Ace of Spades)
+		modelMatrix.Rotate((lastFrameTicks));
+		program.setModelMatrix(modelMatrix);
+		modelMatrix.identity();
+		float vertices2[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices2);
+		glEnableVertexAttribArray(program.positionAttribute);
+		float texCoords2[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+		glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords2);
+		glEnableVertexAttribArray(program.texCoordAttribute);
+		glBindTexture(GL_TEXTURE_2D, text1);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDisableVertexAttribArray(program.positionAttribute);
+		glDisableVertexAttribArray(program.texCoordAttribute);
+
+
+		//Card 3 (Ace of Diamonds)
+		program.setModelMatrix(modelMatrix);
+		float vertices3[] = { 1.0, -0.5, 2.0, -0.5, 2.0, 0.5, 1.0, -0.5, 2.0, 0.5, 1.0, 0.5 };
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices3);	
+		glEnableVertexAttribArray(program.positionAttribute);
+		float texCoords3[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+		glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords3);
+		glEnableVertexAttribArray(program.texCoordAttribute);
+		glBindTexture(GL_TEXTURE_2D, text3);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDisableVertexAttribArray(program.positionAttribute);
+		glDisableVertexAttribArray(program.texCoordAttribute);
+
+		SDL_GL_SwapWindow(displayWindow);
+
+	}
+
+	SDL_Quit();
+	return 0;
+}
